a2aff64a4a9bdc91fca7cefe84937e52f3051337
Denys Stupakov <stupakov106@gmail.com>
2025-08-31 16:22:01 +0200

    Add tailwindcss

---
+++ src/main/resources/skilltrack-frontend/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header>
+        <nav className="flex">
+            <div>
+                SkillTrack
+            </div>
+            <ul>
+                <li>Features</li>
+                <li>Pricing</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
